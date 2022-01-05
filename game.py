00936d4881e300db9418a29c8acf6c7e65ac02c4
Matvey <matvey23lin@gmail.com>
2022-01-05 03:30:17 +0300

    изменение дизайна персонажа и добавление анимаций

--- game.py
+++ game.py
@@ -4,10 +4,8 @@ import pygame
 import random
 
 pygame.init()
-pygame.joystick.init()
-js = pygame.joystick.Joystick(0)
-js.init()
 size = width, height = 750, 450
+fps = 200
 screen = pygame.display.set_mode(size)
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
@@ -55,7 +53,7 @@ tile_images = {
     'empty_1': pygame.transform.scale(load_image('tile_1.png'), (50, 50)),
     'empty_2': pygame.transform.scale(load_image('tile_3.png'), (50, 50))
 }
-player_image = pygame.transform.scale(load_image('creature-1.png'), (40, 40))
+player_sheet = pygame.transform.scale(load_image('knight_sheet.png'), (368, 100))
 dummy_image = load_image('dummy.png')
 rock_image = pygame.transform.scale(load_image('rock.png'), (50, 50))
 rock_break_image = pygame.transform.scale(load_image('rock_break.png'), (50, 50))
@@ -102,13 +100,36 @@ class Rock(pygame.sprite.Sprite):
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, columns, rows, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
-        self.image = player_image
+        self.frames, self.stable_frames, self.moving_frames = [], [], []
+        self.crop_sheet(player_sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
         self.rect = self.image.get_rect()
-        self.rect = self.rect.move(tile_width * pos_x + 5, tile_height * pos_y + 5)
+        self.rect = self.rect.move(tile_width * pos_x + 2, tile_height * pos_y)
+        self.clock = pygame.time.Clock()
+
+    def crop_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for i in range(rows):
+            for j in range(columns):
+                frame_coord = (self.rect.w * j, self.rect.h * i)
+                [self.frames.append(sheet.subsurface(pygame.Rect(frame_coord, self.rect.size)))
+                 for i in range(15)]
+        self.stable_frames = self.frames[:120]
+        self.moving_frames = self.frames[120:]
 
     def update(self, x, y):
+        if moving:
+            self.cur_frame = (self.cur_frame + 1) % len(self.moving_frames)
+            self.image = self.moving_frames[self.cur_frame]
+        else:
+            self.cur_frame = (self.cur_frame + 1) % len(self.stable_frames)
+            self.image = self.stable_frames[self.cur_frame]
+        if x < 0:
+            self.image = pygame.transform.flip(self.image, True, False)
         if pygame.sprite.spritecollideany(self, walls_group) or\
                 pygame.sprite.spritecollideany(self, rocks_group):
             self.rect.x -= x
@@ -136,7 +157,7 @@ class Shot(pygame.sprite.Sprite):
         super().__init__(shot_group)
         self.radius = radius
         self.image = pygame.Surface((2 * radius, 2 * radius), pygame.SRCALPHA, 32)
-        pygame.draw.circle(self.image, pygame.Color('red'), (radius, radius), radius)
+        pygame.draw.circle(self.image, pygame.Color((255, 45, 190)), (radius, radius), radius)
         self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
         self.vx, self.vy = vx, vy
 
@@ -145,6 +166,20 @@ class Shot(pygame.sprite.Sprite):
         if pygame.sprite.spritecollideany(self, enemy_group) and damage or \
                 pygame.sprite.spritecollideany(self, walls_group):
             self.kill()
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = width // 2 - target.rect.x - target.rect.w // 2
+        self.dy = height // 2 - target.rect.y - target.rect.h // 2
 
 
 def generate_level(level):
@@ -172,7 +207,7 @@ def generate_level(level):
                     Tile('wall_8', x, y)
             elif level[y][x] == '@':
                 Tile('empty', x, y)
-                new_player = Player(x, y)
+                new_player = Player(8, 2, x, y)
             elif level[y][x] == '&':
                 Tile('empty', x, y)
                 Enemy(x, y)
@@ -184,14 +219,17 @@ def generate_level(level):
 
 if __name__ == '__main__':
     pygame.display.set_caption('Dungeon Quest: demo')
+
     clock = pygame.time.Clock()
     start_screen()
     player, level_x, level_y = generate_level(load_level('demo.txt'))
     x, y = 0, 0
     v = 1
+    moving = False
     running = True
     while running:
         screen.fill(pygame.Color('black'))
+        moving = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -204,15 +242,14 @@ if __name__ == '__main__':
                     x += v
                 elif event.key == pygame.K_s:
                     y += v
-                moving = True
                 if event.key == pygame.K_LEFT:
-                    shot = Shot(player.rect.x - 14, player.rect.y + 14, 7, -2, 0)
+                    shot = Shot(player.rect.x - 14, player.rect.y + 16, 7, -2, 0)
                 elif event.key == pygame.K_RIGHT:
-                    shot = Shot(player.rect.x + 40, player.rect.y + 14, 7, 2, 0)
+                    shot = Shot(player.rect.x + 46, player.rect.y + 16, 7, 2, 0)
                 elif event.key == pygame.K_UP:
-                    shot = Shot(player.rect.x + 14, player.rect.y - 14, 7, 0, -2)
+                    shot = Shot(player.rect.x + 16, player.rect.y - 16, 7, 0, -2)
                 elif event.key == pygame.K_DOWN:
-                    shot = Shot(player.rect.x + 14, player.rect.y + 40, 7, 0, 2)
+                    shot = Shot(player.rect.x + 16, player.rect.y + 50, 7, 0, 2)
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
                     x += v
@@ -222,6 +259,8 @@ if __name__ == '__main__':
                     x -= v
                 elif event.key == pygame.K_s:
                     y -= v
+        if (x, y) != (0, 0):
+            moving = True
         player.rect.x += x
         player.rect.y += y
         player.update(x, y)
@@ -233,6 +272,6 @@ if __name__ == '__main__':
         player_group.draw(screen)
         shot_group.draw(screen)
         rocks_group.draw(screen)
-        clock.tick(200)
+        clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
