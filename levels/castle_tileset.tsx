8ca055155fdfb109fa5f84a06ea2763780783325
Matvey <matvey23lin@gmail.com>
2022-01-07 03:21:13 +0300

    исправление ошибки

--- levels/castle_tileset.tsx
+++ levels/castle_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="castle_tileset" tilewidth="48" tileheight="48" tilecount="72" columns="9">
- <image source="castle_tileset.png" width="432" height="384"/>
+ <image source="castle_tileset" width="432" height="384"/>
 </tileset>
