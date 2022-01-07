fa763255ff718e8c62871eec6030eca72cba8d5c
Matvey <matvey23lin@gmail.com>
2022-01-08 01:50:51 +0300

    adding a new map and spikes for the minigame

--- game.py
+++ game.py
@@ -4,8 +4,10 @@ import pygame
 import pytmx
 
 pygame.init()
+level = 'castletaker' + input('Введите номер уровня (1, 2): ') + '.tmx'
+maps = {'castletaker1.tmx': [2, 23, (6, 6), (9, 8)], 'castletaker2.tmx': [3, 24, (5, 6), (9, 9)]}
 restart = False
-size = width, height = 432, 384
+size = width, height = 48 * maps[level][3][0], 48 * maps[level][3][1]
 FPS = 120
 MAPS_DIR = 'levels'
 screen = pygame.display.set_mode(size)
@@ -13,8 +15,8 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 slimes_group = pygame.sprite.Group()
 tables_group = pygame.sprite.Group()
+spikes_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
-kill = []
 
 
 def load_image(name, colorkey=None):
@@ -35,7 +37,8 @@ def load_image(name, colorkey=None):
 
 player_sheet = pygame.transform.scale(load_image('knight_sheet.png'), (368, 96))
 slime_sheet = load_image('slime_idle_spritesheet.png')
-floor = [15, 21, 22, 23, 24, 30, 31, 32, 33]
+spikes_images = ['holes.png', 'spikes.png', 'on_holes.png', 'on_spikes.png']
+floor = [6, 15, 21, 22, 23, 24, 30, 31, 32, 33]
 ts = tile_width = tile_height = 48
 player = None
 
@@ -61,9 +64,11 @@ class Dungeon:
                 self.player_x, self.player_y = obj.x // tile_width, obj.y // tile_height
             elif obj.type == 'slime':
                 Slime(6, 1, obj.x // tile_width, obj.y // tile_height)
+            elif obj.type == 'spike':
+                Spikes(obj.x // tile_width, obj.y // tile_height)
 
     def render(self):
-        for i in range(2):
+        for i in range(maps[level][0]):
             for y in range(self.height):
                 for x in range(self.width):
                     image = self.map.get_tile_image(x, y, i)
@@ -101,7 +106,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect = self.rect.move(tile_width * pos_x + 1, tile_height * pos_y)
         self.mask = pygame.mask.from_surface(self.image)
-        self.attempt = 23
+        self.attempt = maps[level][1]
+        self.color = 'white'
 
     def crop_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -113,17 +119,31 @@ class Player(pygame.sprite.Sprite):
                  for i in range(6)]
 
     def update(self, x, y, flip, back):
+        damage = 0
         self.cur_frame = (self.cur_frame + 1) % len(self.frames[:self.half_frames])
         self.image = self.frames[:self.half_frames][self.cur_frame]
+        self.pos_x += x // ts
+        self.pos_y += y // ts
         if moving:
-            self.attempt -= 1
+            damage += 1
         if flip:
             self.image = pygame.transform.flip(self.image, True, False)
         if dungeon.get_tile_id((self.rect.x // 48, self.rect.y // 48), 0) not in floor or back:
             self.rect.x -= x
             self.rect.y -= y
-            self.attempt += 1
-        draw(screen, self.attempt, (self.rect.x // ts, self.rect.y // ts))
+            self.pos_x = self.rect.x // ts
+            self.pos_y = self.rect.y // ts
+            damage -= 1
+        if damage == 1:
+            self.color = 'white'
+        if ((self.pos_x, self.pos_y) in [(s.rect.x // ts, s.rect.y // ts) for s in spikes_group] and
+            damage == 1 and not pygame.sprite.spritecollideany(self, tables_group or slimes_group))\
+                or (back and (self.pos_x, self.pos_y) in [(s.rect.x // ts, s.rect.y // ts)
+                                                          for s in spikes_group]):
+            damage += 1
+            self.color = 'red'
+        self.attempt -= damage
+        draw(screen, self.attempt, (self.rect.x // ts, self.rect.y // ts), self.color)
 
 
 class Slime(pygame.sprite.Sprite):
@@ -177,24 +197,44 @@ class Table(pygame.sprite.Sprite):
             self.rect.y += y
             touch += 1
             player_group.update(x, y, flip, True)
-        if (dungeon.get_tile_id((self.rect.x // ts, self.rect.y // ts), 0) not in floor or
-            dungeon.get_tile_id((self.rect.x // ts, self.rect.y // ts), 1) == floor[0])\
-                and touch >= 1:
+        if (dungeon.get_tile_id((self.rect.x // ts, self.rect.y // ts),
+                                0) not in floor or (
+                (self.rect.x // ts, self.rect.y // ts) in
+                [(spike_sprite.rect.x // ts, spike_sprite.rect.y // ts)
+                 for spike_sprite in tables_group if self != spike_sprite] and touch >= 1) or
+                ((self.rect.x // ts, self.rect.y // ts) in
+                 [(round(slimes_sprite.rect.x / ts), round(slimes_sprite.rect.y / ts))
+                  for slimes_sprite in slimes_group])):
             self.rect.x -= x
             self.rect.y -= y
 
 
-def draw(screen, text, position):
+class Spikes(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(spikes_group, all_sprites)
+        self.pos_x, self.pos_y = pos_x, pos_y
+        self.image = load_image(spikes_images[1])
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(tile_width * pos_x, tile_height * pos_y)
+
+    def update(self):
+        self.image = load_image(spikes_images[1])
+        if (self.pos_x, self.pos_y) in [(s.rect.x // ts, s.rect.y // ts) for s in tables_group] or\
+                pygame.sprite.spritecollideany(self, player_group):
+            self.image = load_image(spikes_images[3])
+
+
+def draw(screen, text, position, color):
     global restart
     if text == 0:
         text = 'X'
     elif text < 0:
         restart = True
-    if position == (6, 6):
+    if position == maps[level][2]:
         text = 'WIN'
     screen.fill((0, 0, 0))
     font = pygame.font.Font(None, 50)
-    text = font.render(str(text), True, (255, 255, 255))
+    text = font.render(str(text), True, color)
     text_x = 10
     text_y = 10
     screen.blit(text, (text_x, text_y))
@@ -203,7 +243,7 @@ def draw(screen, text, position):
 if __name__ == '__main__':
     pygame.display.set_caption('Dungeon Quest: feature')
 
-    dungeon = Dungeon('castletaker1.tmx')
+    dungeon = Dungeon(level)
     clock = pygame.time.Clock()
     pygame.mouse.set_visible(False)
 
@@ -214,12 +254,13 @@ if __name__ == '__main__':
     moving = False
     flip = False
     running = True
+    color = 'white'
     while running:
         screen.fill(pygame.Color((0, 0, 0)))
         if restart:
             for sprite in all_sprites:
                 sprite.kill()
-            dungeon = Dungeon('castletaker1.tmx')
+            dungeon = Dungeon(level)
             player_x, player_y = dungeon.render()
             player = Player(8, 2, player_x, player_y)
             restart = False
@@ -250,11 +291,13 @@ if __name__ == '__main__':
         player.update(x, y, flip, False)
         slimes_group.update(x, y, flip)
         tables_group.update(x, y, flip)
+        spikes_group.update()
 
         tiles_group.draw(screen)
         slimes_group.draw(screen)
         tables_group.draw(screen)
         player_group.draw(screen)
+        spikes_group.draw(screen)
         clock.tick(FPS)
         pygame.display.flip()
     pygame.quit()
